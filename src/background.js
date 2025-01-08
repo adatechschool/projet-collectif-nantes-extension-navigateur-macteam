56dfcf39a9a1ff0c6b434ea5b4512c74e0997599
Ange230700 <kouakouangeericstephane@gmail.com>
2025-01-08 11:21:57 +0100

    création fonctionnalité permettant de sélectionner un texte

--- src/background.js
+++ src/background.js
@@ -1,10 +1,14 @@
+
+// Permet de visualiser le bouton "help IA" dans le menu contextuel
 chrome.runtime.onInstalled.addListener(() => {
   chrome.contextMenus.create({
     id: "button",
     title: "Help IA",
-    contexts: ["selection"],
+    contexts: ["all"],
   });
 });
+
+
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
   if (info.menuItemId === "button") {
@@ -13,15 +17,15 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
       func: () => {
-          // récupérer le texte en surbrillance
-          document.addEventListener("selectionchange", () => {
-              const selection = window.getSelection()
-              const selectionText = selection.toString().trim()
-              if (selectionText) {
-                  console.log(selectionText)
-              }
-          })
+        // récupérer le texte en surbrillance
+        const selection = window.getSelection()
+        const selectionText = selection.toString().trim()
+        if (selectionText) {
+          console.log(selectionText)
+        }
       },
     });
   }
 });
+
+
