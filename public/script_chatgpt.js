98b8bfc6914fdcfa520f67802cf3ec9fd75e79ac
Ange230700 <kouakouangeericstephane@gmail.com>
2025-01-14 16:42:04 +0100

    on a fini le projet

--- public/script_chatgpt.js
+++ public/script_chatgpt.js
@@ -1,12 +1,14 @@
 // public\script_chatgpt.js
 
+// Une fois arrivé sur chatgpt, on répète de la logique de notre extension pendant un intervalle de temps 
 const intervalId = setInterval(async () => {
     const textarea = document.querySelector("#prompt-textarea p");
 
     if (textarea) {
+        // On récupère les données enregistrées dans le stockage local
         const result = await chrome.storage.local.get(["help_IA_text", "help_IA_mode"]);
         const { help_IA_text, help_IA_mode } = result;
-
+       // On injecte le texte sélectionné et l'instruction dans la zone de texte de chatgpt
         if (help_IA_text) {
             let promptToInject = help_IA_text;
 
@@ -17,16 +19,18 @@ const intervalId = setInterval(async () => {
             }
 
             textarea.innerText = promptToInject;
+            // On déclenche l'événement input pour que le texte soit injecté dans le textarea de chatgpt
             textarea.dispatchEvent(new Event("input", { bubbles: true }));
-
+            // On attend un certain temps pour que le bouton de chatgpt s'active
             await new Promise((resolve) => setTimeout(resolve, 700));
 
             const sendButton = document.querySelector('div.flex.gap-x-1 > button');
+
             if (sendButton && !sendButton.disabled) {
                 setTimeout(() => sendButton.click(), 1000);
-                // sendButton.click();
+                
             }
-
+            // On vide le texte sélectionné de notre stockage local 
             await chrome.storage.local.remove("help_IA_text");
         }
 
@@ -34,4 +38,5 @@ const intervalId = setInterval(async () => {
     }
 }, 500);
 
+// Au bout de 10s si l'execution de l'extension est obsolete, on arrête le script
 setTimeout(() => clearInterval(intervalId), 10000);
--- src/script.js
+++ src/script.js
@@ -1,21 +1,23 @@
 // src\script.js
 
+// Initialisation du popup et du selectionText(contenu du texte surligné)
 let popup = null;
 let selectionText;
 
+// Gestion du surlignage à l'aide la souris  
 document.addEventListener("mouseup", (e) => {
-    if (popup && popup.contains(e.target)) {
+    if (popup && popup.contains(e.target)) {   // Si un popup est deja visible, on bloque l'apparition d'un nouveau popup
         return;
     }
 
-    selectionText = window.getSelection().toString();
+    selectionText = window.getSelection().toString().trim();  // On récupère le texte sélectionné et on le met dans selectionText
 
-    if (!selectionText) {
+    if (!selectionText) {  // Si aucun texte n'est sélectionné, on retire le popup 
         removePopup();
         return;
     }
 
-    createPopup(e);
+    createPopup(e);    
 });
 
 function createPopup(e) {
@@ -23,16 +25,23 @@ function createPopup(e) {
         removePopup();
     }
 
+    // On crée le popup
     popup = document.createElement("div");
     popup.classList.add("help-ia-popup");
-
+    
+    // On positionne le popup à l'endroit de la souris
     popup.style.position = "absolute";
     popup.style.top = `${e.pageY + 10}px`;
     popup.style.left = `${e.pageX}px`;
+   // Création de l'icone du popup
+    const icon = document.createElement("img");
+    icon.src = "https://github.com/adatechschool/projet-collectif-nantes-extension-navigateur-macteam/blob/feature/popDisplay/public/Icons/icon128.png?raw=true";
+    icon.alt = "Help IA icon";
 
+    popup.appendChild(icon);
+// Création des boutons du popup 
     const summarizeBtn = document.createElement("button");
-    summarizeBtn.innerText = "Résumé";
-    summarizeBtn.style.marginRight = "10px";
+    summarizeBtn.innerText = "Résumer";
     summarizeBtn.addEventListener("click", () => handleClick("summary"));
 
     const explainBtn = document.createElement("button");
@@ -41,23 +50,30 @@ function createPopup(e) {
 
     popup.appendChild(summarizeBtn);
     popup.appendChild(explainBtn);
-
+// On ajoute le popup au body de la page
     document.body.appendChild(popup);
+
+    // On crée un effet de transition pour le popup
+    setTimeout(() => {
+        popup.classList.add('show');
+    }, 10);
 }
 
 function removePopup() {
     if (popup) {
-        popup.remove();
-        popup = null;
+        popup.classList.remove('show');
+            popup.remove();
+            popup = null;
     }
 }
 
 function handleClick(mode) {
+    // On enregistre le mode de l'extension IA et le texte sélectionné dans le stockage local
     chrome.storage.local.set({
         help_IA_text: selectionText,
         help_IA_mode: mode
     });
-
+// On bascule vers la page de chatGPT dans un nouvel onglet
     window.open("https://www.chatgpt.com", "_blank");
 
     removePopup();
