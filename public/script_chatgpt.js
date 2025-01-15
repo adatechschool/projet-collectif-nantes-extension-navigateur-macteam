a543093a07b0e82649276968252ad63197c672f3
Ange230700 <kouakouangeericstephane@gmail.com>
2025-01-15 12:50:45 +0100

    on a fait de la refactorization

--- public/script_chatgpt.js
+++ public/script_chatgpt.js
@@ -1,5 +1,27 @@
 // public\script_chatgpt.js
+function selectMode(mode, promptToInject) {
+    if (mode === "explain") {
+        promptToInject += "\n\n  Instruction: Fais-moi une explication de ce texte.";
+    } else if (mode === "summary") {
+        promptToInject += "\n\n  Instruction: Fais-moi le résumé de ce texte.";
+    }
 
+    return promptToInject;
+}
+
+function injectSelectedText(promptToInject, textarea) {
+    textarea.innerText = promptToInject;
+    // On déclenche l'événement input pour que le texte soit injecté dans le textarea de chatgpt
+    textarea.dispatchEvent(new Event("input", { bubbles: true }));
+}
+
+function sendButtonClick() {
+    const sendButton = document.querySelector('div.flex.gap-x-1 > button');
+
+    if (sendButton && !sendButton.disabled) {
+        setTimeout(() => sendButton.click(), 700);
+    }
+}
 // Une fois arrivé sur chatgpt, on répète de la logique de notre extension pendant un intervalle de temps 
 const intervalId = setInterval(async () => {
     const textarea = document.querySelector("#prompt-textarea p");
@@ -8,28 +30,17 @@ const intervalId = setInterval(async () => {
         // On récupère les données enregistrées dans le stockage local
         const result = await chrome.storage.local.get(["help_IA_text", "help_IA_mode"]);
         const { help_IA_text, help_IA_mode } = result;
-       // On injecte le texte sélectionné et l'instruction dans la zone de texte de chatgpt
+        // On injecte le texte sélectionné et l'instruction dans la zone de texte de chatgpt
         if (help_IA_text) {
             let promptToInject = help_IA_text;
 
-            if (help_IA_mode === "explain") {
-                promptToInject += " Fais-moi une explication de ce texte.";
-            } else if (help_IA_mode === "summary") {
-                promptToInject += " Fais-moi le résumé de ce texte.";
-            }
+            promptToInject = selectMode(help_IA_mode, promptToInject);
+            injectSelectedText(promptToInject, textarea);
 
-            textarea.innerText = promptToInject;
-            // On déclenche l'événement input pour que le texte soit injecté dans le textarea de chatgpt
-            textarea.dispatchEvent(new Event("input", { bubbles: true }));
             // On attend un certain temps pour que le bouton de chatgpt s'active
             await new Promise((resolve) => setTimeout(resolve, 700));
+            sendButtonClick()
 
-            const sendButton = document.querySelector('div.flex.gap-x-1 > button');
-
-            if (sendButton && !sendButton.disabled) {
-                setTimeout(() => sendButton.click(), 1000);
-                
-            }
             // On vide le texte sélectionné de notre stockage local 
             await chrome.storage.local.remove("help_IA_text");
         }
--- src/script.js
+++ src/script.js
@@ -1,80 +1,25 @@
 // src\script.js
 
+import { createPopup, removePopup } from "./javascript/popup.js";
+
 // Initialisation du popup et du selectionText(contenu du texte surligné)
-let popup = null;
-let selectionText;
+export const variables = {
+    popup: null,
+    selectionText: null,
+};
 
 // Gestion du surlignage à l'aide la souris  
 document.addEventListener("mouseup", (e) => {
-    if (popup && popup.contains(e.target)) {   // Si un popup est deja visible, on bloque l'apparition d'un nouveau popup
+    if (variables.popup && variables.popup.contains(e.target)) {   // Si un popup est deja visible, on bloque l'apparition d'un nouveau popup
         return;
     }
 
-    selectionText = window.getSelection().toString().trim();  // On récupère le texte sélectionné et on le met dans selectionText
+    variables.selectionText = window.getSelection().toString().trim();  // On récupère le texte sélectionné et on le met dans selectionText
 
-    if (!selectionText) {  // Si aucun texte n'est sélectionné, on retire le popup 
+    if (!variables.selectionText) {  // Si aucun texte n'est sélectionné, on retire le popup 
         removePopup();
         return;
     }
 
-    createPopup(e);    
+    createPopup(e); 
 });
-
-function createPopup(e) {
-    if (popup) {
-        removePopup();
-    }
-
-    // On crée le popup
-    popup = document.createElement("div");
-    popup.classList.add("help-ia-popup");
-    
-    // On positionne le popup à l'endroit de la souris
-    popup.style.position = "absolute";
-    popup.style.top = `${e.pageY + 10}px`;
-    popup.style.left = `${e.pageX}px`;
-   // Création de l'icone du popup
-    const icon = document.createElement("img");
-    icon.src = "https://github.com/adatechschool/projet-collectif-nantes-extension-navigateur-macteam/blob/feature/popDisplay/public/Icons/icon128.png?raw=true";
-    icon.alt = "Help IA icon";
-
-    popup.appendChild(icon);
-// Création des boutons du popup 
-    const summarizeBtn = document.createElement("button");
-    summarizeBtn.innerText = "Résumer";
-    summarizeBtn.addEventListener("click", () => handleClick("summary"));
-
-    const explainBtn = document.createElement("button");
-    explainBtn.innerText = "Expliquer";
-    explainBtn.addEventListener("click", () => handleClick("explain"));
-
-    popup.appendChild(summarizeBtn);
-    popup.appendChild(explainBtn);
-// On ajoute le popup au body de la page
-    document.body.appendChild(popup);
-
-    // On crée un effet de transition pour le popup
-    setTimeout(() => {
-        popup.classList.add('show');
-    }, 10);
-}
-
-function removePopup() {
-    if (popup) {
-        popup.classList.remove('show');
-            popup.remove();
-            popup = null;
-    }
-}
-
-function handleClick(mode) {
-    // On enregistre le mode de l'extension IA et le texte sélectionné dans le stockage local
-    chrome.storage.local.set({
-        help_IA_text: selectionText,
-        help_IA_mode: mode
-    });
-// On bascule vers la page de chatGPT dans un nouvel onglet
-    window.open("https://www.chatgpt.com", "_blank");
-
-    removePopup();
-}
